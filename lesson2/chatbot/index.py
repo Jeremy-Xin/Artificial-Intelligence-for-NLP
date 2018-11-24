1c7947099b70ddaec1d95c9f2d6464d41200a2d8
Jeremy-Xin <Jeremy-Xin@users.noreply.github.com>
2018-11-24 21:13:20 +0800

    lession 2 assignment/Users/xincj/.virtualenvs/python3env/bin/python

---
+++ lesson2/chatbot/index.py
@@ -0,0 +1,18 @@
+from eliza import start_conversation
+
+inputs = [	
+	'hello there',
+	'I want to test this program',
+	'I could see if it works',
+	'no not really',
+	'no',
+	'forget it-- I was wondering how general the program is',
+	'I feel like it',
+	'I feel this is enough',
+	'Shit',
+	'WTF',
+	'I want my mother give me some money',
+]
+
+if __name__ == '__main__':
+	start_conversation(inputs)
